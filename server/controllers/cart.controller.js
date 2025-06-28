e332c678e75a845a76e4ade596832fcb57411a2b
raju <gnraju0076@gmail.com>
2025-06-28 14:48:17 +0530

    server API s modified

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,34 +1,35 @@
-import { Cart } from "../models/cart.model.js"
+import { Cart } from "../models/cart.model.js";
+import { calculateDiscounts } from "../services/discount.service.js";
 
 
 
-export const addToCart=async(req,res)=>{
-  const {productId,quantity=1}=req.body
-  let cart = await Cart.findOne()
+export const addToCart = async (req, res) => {
+  const { productId, quantity = 1 } = req.body;
 
-  if(!cart){
-    cart=new Cart({items:[]})
+  let cart = await Cart.findOne();
+
+  if (!cart) {
+    cart = new Cart({ items: [] });
   }
 
-  const index=cart.findIndex(item=>item.product.equals(productId))
-  if(index !== -1){
-    cart.items[index].quantity +=quantity
-  }
-  else{
-    cart.items.push({product:productId,quantity})
-  }
-  await cart.save()
-  res.status(200).json(cart)
-}
+  const index = cart.items.findIndex(item => item.product.equals(productId));
 
+  if (index !== -1) {
+    cart.items[index].quantity += quantity
+  } else {
+    cart.items.push({ product: productId, quantity })
+  }
 
+  await cart.save();
+  res.status(200).json({ message: "Item added to cart", cart })
+};
 
 export const getCart = async (req, res) => {
   const cart = await Cart.findOne().populate("items.product");
 
-  if (!cart) return res.status(404).json({ message: "Cart is empty" })
+  if (!cart) return res.status(404).json({ message: "Cart is empty" });
 
-  const { total, discounts } = calculateDiscounts(cart.items)
+  const { total, discounts } = calculateDiscounts(cart.items);
 
   res.json({
     items: cart.items.map(i => ({
